2610ec69d0fe25d78c1f667354a705332410f2d3
abedayoub <70668438+abedayoub@users.noreply.github.com>
2020-12-15 13:34:11 +0200

    Update get Data.py

--- get Data.py
+++ get Data.py
@@ -1,15 +1,13 @@
-import serial 
 import time
 import pymysql
 pymysql.install_as_MySQLdb()
 import MySQLdb
 import matplotlib.pyplot as plt
-import numpy as np
 from collections import namedtuple
 import pandas as pd
-import pyodbc
 
-dbConn = MySQLdb.connect("localhost","root","","tempdb")
+
+dbConn = MySQLdb.connect("localhost","root","","tempdb") #or MySQLdb.die("can't")
 
 cursor = dbConn.cursor()
         
@@ -25,44 +23,3 @@ df.plot(ax=ax)
 print(df.describe())
 print(df.head())
 dbConn.close()
-
-
- 
-#data = cursor.fetchall()
-
-
-
-# close cursor and connection
-#cursor.close()
-#dbConn.close()
-
-# unpack data in TimeStamp (x axis) and Pac (y axis)
-#Date, Temperature = zip(*data)
-
-# graph code
-#plt.plot(Date, Temperature, align='center')
-
-# set title, X/Y labels
-#plt.title("Plotting Test")
-#plt.xlabel("Date")
-#plt.ylabel("Temperature")
-#fig = plt.gcf()
-
-# plt.xticks(TimeStamp, (hour))
-#fig.set_size_inches(20.5,10.5)
-#plt.grid(True)
-#plt.draw()
-#fig.savefig('test2.png', dpi=100)
-
-   
-#for x in range(len(path_exec)):
-#    string'x' = "Hello"
-#    print(dataRow3)
-
-#for x in range(len(path_exec)):
-#    pieces = dataRow.split()
-    
-
-
-#for i in range(len(path_exec[0])):
-    
